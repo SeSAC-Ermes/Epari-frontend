cdf2435999f4b4e443df064b52354eef37ff5cac
liimjiin <limiary27@gmail.com>
2024-10-30 15:58:49 +0900

    INIT: 프로젝트 frontend 초기 설정 - React 프로젝트 기본 구조 생성 - 기본 디렉토리 구조 생성 - Tailwind CSS 추가 및 설정 - package.json 및 종속성 설정 - gitignore 파일 설정

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../assets/epariLogo.jpg';
+
+function Home() {
+  return (
+      <>
+        <img src={Logo} alt="Epari Logo"/>
+        <h1>Home Page</h1>
+      </>
+  );
+}
+
+export default Home;
