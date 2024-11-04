467e4a389953b54d29343364412761d2df2808c5
jjong <whdgh9588@gmail.com>
2024-11-04 20:54:57 +0900

    FEAT: 라우팅 설정 업데이트 및 새로운 페이지 추가
    - 추가된 페이지는 CourseDetailPage, LectureNoticeListPage, QnAListPage, CurriculumPage, AccountPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ExamPage from './pages/exam/ExamPage';
 import React from 'react';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import CourseList from "./pages/CourseList.jsx";
+import SignInPage from './pages/SignInPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import CourseListPage from "./pages/CourseListPage.jsx";
+import CourseDetailPage from "./pages/CourseDetailPage.jsx";
+import LectureNoticeListPage from "./pages/LectureNoticeListPage.jsx";
+import QnAListPage from "./pages/QnAListPage.jsx";
+import CurriculumPage from "./pages/CurriculumPage.jsx";
+import AccountPage from "./pages/AccountPage.jsx";
 
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="/exams" element={<ExamPage />} />
-          <Route path="/signin" element={<SignIn/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/courselist" element={<CourseList/>}/>
+          <Route path="/signin" element={<SignInPage/>}/>
+          <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="/courselist" element={<CourseListPage/>}/>
+          <Route path="/coursedetail" element={<CourseDetailPage />} />
+          <Route path="/lecturenoticelist" element={<LectureNoticeListPage />} />
+          <Route path="/qnalist" element={<QnAListPage />} />
+          <Route path="/curriculum" element={<CurriculumPage />} />
+          <Route path="/account" element={<AccountPage />} />
         </Routes>
       </Router>
   );
