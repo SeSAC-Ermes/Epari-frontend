d0b0fa177d7dd978ab31a3fe57f07e28cca2442f
씅찬 <151029131+sseung519@users.noreply.github.com>
2024-11-04 17:37:21 +0900

    [FEAT] 사이드바 가이드 (#9)
    사이드바 초안 가이드

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ExamPage from './pages/exam/ExamPage';
 
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/exams" element={<ExamPage />} />
         </Routes>
       </Router>
   );
