b31dd82b2ab23746f3100fe15256f72b654ab3dc
liimjiin <limiary27@gmail.com>
2024-11-05 17:48:13 +0900

    FEAT: amplify-config 업데이트 - Cognito 서비스 명시적 지정 - 로그인 방식 설정 추가 (이메일 허용) - 로그아웃 리다이렉트 URL 수정

--- src/config/amplify-config.js
+++ src/config/amplify-config.js
@@ -2,15 +2,22 @@ import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
   Auth: {
-    region: 'ap-northeast-2',
-    userPoolId: 'ap-northeast-2_pD4FKh81Z',
-    userPoolWebClientId: '57kqtimedjbhgnvt3e2msercue',
-    oauth: {
-      domain: '202533495554epari-pool.auth.ap-northeast-2.amazoncognito.com',
-      scope: ['email', 'profile', 'openid'],
-      redirectSignIn: 'http://localhost:5173/courselist',
-      redirectSignOut: 'http://localhost:3000',
-      responseType: 'code'
+    Cognito: {
+      userPoolId: 'ap-northeast-2_pD4FKh81Z',
+      userPoolClientId: '3qonkcahudsvh9eathk7h6v9su',
+      region: 'ap-northeast-2',
+      loginWith: {
+        email: true,
+        phone: false,
+        username: false
+      },
+      oauth: {
+        domain: '202533495554epari-pool.auth.ap-northeast-2.amazoncognito.com',
+        scopes: ['email', 'profile', 'openid'],
+        responseType: 'code',
+        redirectSignIn: 'http://localhost:5173/courselist',
+        redirectSignOut: 'http://localhost:5173'
+      }
     }
   }
-})
+});
