7da7be0added38c6340bd8c2dba579d73fd5e989
Chan-GN <ocg0229@gmail.com>
2024-11-14 20:43:00 +0900

    FIX: 일부 경로 수정
    - Route 수정으로 인한 동작 이상을 해결하기 위해 일부 경로를 수정

--- src/pages/lecture/CourseFilePage.jsx
+++ src/pages/lecture/CourseFilePage.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import CourseFileContent from '../../components/lecture/file/CourseFileContent.jsx';
-import { useSearchParams } from 'react-router-dom';
 
 const CourseFilePage = () => {
-  const [searchParams] = useSearchParams();
-  const courseId = searchParams.get('id');
-
   return (
-      <CourseFileContent courseId={courseId}/>
+      <CourseFileContent/>
   );
 };
 
