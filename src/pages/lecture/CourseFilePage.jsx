9764c8fd0d82c6613e6496b6da87c5e09096c743
Oh Changeun <111508505+Chan-GN@users.noreply.github.com>
2024-11-14 21:09:36 +0900

    [REFACTOR] 탑바, 사이드바 리팩토링 (#67)
    - 공통 레이아웃(MainLayout, SimpleLayout) 구현 및 중첩 라우팅 구조 개선
    - 사용자 역할(강사/학생)별 사이드바 메뉴 구조 재설계 및 기능 추가
    - 학습 관리 페이지(수강생 관리, 학습현황) 기본 구조 구현
    - TopBar와 Sidebar 레이아웃 구조 최적화 및 반응형 대응
    - 각종 라우팅 경로 수정 및 컴포넌트 CSS 버그 수정

--- src/pages/lecture/CourseFilePage.jsx
+++ src/pages/lecture/CourseFilePage.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import TopBar from '../../components/layout/TopBar.jsx';
-import Sidebar from '../../components/layout/Sidebar.jsx';
 import CourseFileContent from '../../components/lecture/file/CourseFileContent.jsx';
-import { useSearchParams } from 'react-router-dom';
 
 const CourseFilePage = () => {
-  const [searchParams] = useSearchParams();
-  const courseId = searchParams.get('id');
-
   return (
-      <div className="min-h-screen bg-gray-50 flex">
-        <Sidebar />
-        <div className="flex-1">
-          <TopBar />
-          <CourseFileContent courseId={courseId} />
-        </div>
-      </div>
+      <CourseFileContent/>
   );
 };
 
