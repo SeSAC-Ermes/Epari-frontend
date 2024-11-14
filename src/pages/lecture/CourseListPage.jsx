59f3a20fc93f788fd3200ef06191b17d70082df1
Chan-GN <ocg0229@gmail.com>
2024-11-14 19:51:01 +0900

    FIX: CourseListPage CSS 수정
    - TopBar 컴포넌트의 높이만큼 패딩 값을 부여

--- src/pages/lecture/CourseListPage.jsx
+++ src/pages/lecture/CourseListPage.jsx
@@ -3,7 +3,7 @@ import CourseListContent from '../../components/lecture/dashboard/CourseListCont
 
 const CourseListPage = () => {
   return (
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-gray-50 pt-16">
         <CourseListContent/>
       </div>
   );
