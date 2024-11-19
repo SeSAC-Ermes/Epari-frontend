9669e49a115155f74f4cca21f8bb6f4609891a03
12ka39 <work_lee2@naver.com>
2024-11-19 20:21:07 +0900

    FEAT: 강사만 글작성 가능하도록 권한 수정 - 글작성 버튼 권한 수정 - 글작성 페이지도 강사만 보이도록 권한 수정

--- src/pages/notice/NoticeWritePage.jsx
+++ src/pages/notice/NoticeWritePage.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import TopBar from '../../components/layout/TopBar';
 import NoticeWriteContent from "../../components/notice/NoticeWriteContent.jsx";
 import { useAuth } from '../../auth/AuthContext';
-
-
+import { withPageAuth } from "../../auth/WithAuth.jsx";
 
 const NoticeWritePage = () => {
 
   return (
       <div className="min-h-screen bg-gray-50 flex">
         <div className="flex-1">
-          <TopBar />
-          <NoticeWriteContent type="GLOBAL" />
+          <TopBar/>
+          <NoticeWriteContent type="COURSE"/>
         </div>
       </div>
   );
 };
 
-export default NoticeWritePage;
+export default withPageAuth(NoticeWritePage, 'CREATE_NOTICE_COURSE');
