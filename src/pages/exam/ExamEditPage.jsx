c3cc49e06385385f501e704e27debd9e9339a7a0
jjong <whdgh9588@gmail.com>
2024-11-21 01:35:55 +0900

    FEAT: ExamEditPage 컴포넌트 추가
    - 강사 권한을 가진 사용자만 접근 가능한 시험 수정 페이지 구현

---
+++ src/pages/exam/ExamEditPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ExamEdit from '../../components/exam/ExamEdit';
+import { withPageAuth } from '../../auth/WithAuth.jsx';
+
+const ExamEditPage = () => {
+  return (
+      <div className="min-h-screen bg-gray-50 flex-1 overflow-y-auto">
+        <main className="max-w-7xl mx-auto px-4 py-8">
+          <ExamEdit/>
+        </main>
+      </div>
+  );
+}
+
+export default withPageAuth(ExamEditPage, 'ROLES.INSTRUCTOR');
