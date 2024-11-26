ab929d035904d982570b61c8417f612f3ce39c77
liimjiin <limiary27@gmail.com>
2024-11-26 18:30:51 +0900

    FEAT: 개발 / 배포용 환경변수 설정

--- src/config/amplify-config.js
+++ src/config/amplify-config.js
@@ -1,27 +1,47 @@
 import { Amplify } from 'aws-amplify';
 
-const redirectUrls = {
-  signIn: [import.meta.env.VITE_REDIRECT_SIGNIN],
-  signOut: [import.meta.env.VITE_REDIRECT_SIGNOUT]
+const envConfig = {
+  region: 'ap-northeast-2',
+  redirectUrls: {
+    signIn: import.meta.env.VITE_REDIRECT_SIGNIN,
+    signOut: import.meta.env.VITE_REDIRECT_SIGNOUT,
+    // Production에서만 사용되는 www 도메인 URL
+    signInWWW: import.meta.env.VITE_REDIRECT_SIGNIN_WWW,
+    signOutWWW: import.meta.env.VITE_REDIRECT_SIGNOUT_WWW
+  }
 };
 
-if (import.meta.env.PROD) {
-  redirectUrls.signIn.push(import.meta.env.VITE_REDIRECT_SIGNIN_WWW);
-  redirectUrls.signOut.push(import.meta.env.VITE_REDIRECT_SIGNOUT_WWW);
-}
+const getRedirectUrls = (baseUrl, wwwUrl) => {
+  const urls = [baseUrl];
+  if (import.meta.env.PROD && wwwUrl) {
+    urls.push(wwwUrl);
+  }
+  return urls;
+};
 
 Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: import.meta.env.VITE_USER_POOL_ID,
       userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
-      region: 'ap-northeast-2',
+      region: envConfig.region,
       loginWith: {
         oauth: {
           domain: import.meta.env.VITE_COGNITO_DOMAIN,
-          scopes: ['email', 'profile', 'openid'],
-          redirectSignIn: redirectUrls.signIn.filter(Boolean),
-          redirectSignOut: redirectUrls.signOut.filter(Boolean),
+          scopes: [
+            'email',
+            'profile',
+            'openid',
+            'aws.cognito.signin.user.admin'
+          ],
+          redirectSignIn: getRedirectUrls(
+              envConfig.redirectUrls.signIn,
+              envConfig.redirectUrls.signInWWW
+          ),
+          redirectSignOut: getRedirectUrls(
+              envConfig.redirectUrls.signOut,
+              envConfig.redirectUrls.signOutWWW
+          ),
           responseType: 'code',
           providers: ['Google']
         },
