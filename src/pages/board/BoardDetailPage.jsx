99ba81342d900940f6e11c3f98de847c5066729e
jjong <whdgh9588@gmail.com>
2025-01-02 22:33:23 +0900

    FEAT: 게시글 상세 페이지 컴포넌트 생성
    - 게시글 상세 페이지를 위한 BoardDetailPage 컴포넌트를 생성했습니다.

---
+++ src/pages/board/BoardDetailPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BoardDetailContent from '../../components/board/BoardDetailContent';
+
+const BoardDetailPage = () => {
+  return (
+      <BoardDetailContent />
+  );
+};
+
+export default BoardDetailPage;
